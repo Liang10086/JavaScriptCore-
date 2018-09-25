5c14629a540e71064f9f8a70aabc7ebe07a34c68
wangmingliang <wangmingliang@idengyun.com>
2018-09-25 14:02:19 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 王明亮 on 2018/9/25.
+//  Copyright © 2018年 王明亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by 王明亮 on 2018/9/25.
+//  Copyright © 2018年 王明亮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
